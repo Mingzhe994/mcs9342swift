4f4200dd9eba318496f21fc9a761e59f12dae402
Stuart <tuzuki@126.com>
2016-03-14 09:47:05 +1100

    lab1

---
+++ Magic 8 Ball/EightBallModel.swift
@@ -0,0 +1,45 @@
+//
+//  EightBallModel.swift
+//  Magic 8 Ball
+//
+//  Created by Stuart on 8/03/2016.
+//  Copyright © 2016 Stuart. All rights reserved.
+//
+
+import Foundation
+
+class EightBallModel:CustomStringConvertible,CustomDebugStringConvertible {
+    static let initailRespenseArray = ["🐎","Mom"]
+    
+    var responseArray :[String]?
+    
+    init(){
+        responseArray = EightBallModel.initailRespenseArray
+    }
+    
+    init(extraResponseArray:Array<String>){
+        responseArray = EightBallModel.initailRespenseArray + extraResponseArray
+    }
+    
+    var description :String{
+        var 😃 = ""
+        //Use a for loop to create a string based on the elements inside of response array
+        for response in responseArray!{
+            😃 += response
+        }
+        return 😃
+    }
+    
+    var debugDescription:String{
+        //Use a for loop to create a string based on the elements inside of response array 
+        //Make sure the string starts with the word "Debug:"
+        var str = "Debug: "
+        //Use a for loop to create a string based on the elements inside of response array
+        for response in responseArray!{
+            str += response
+        }
+        return str
+    }
+}
+
+
--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,6 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Name: Mingzhe")
+        
+        let age:Double = 28
+        print("Age: ", age)
+        
+        let name = "Mingzhe"
+        
+        print("Name: ", name)
+    
+        
+        let extraR = ["God father","good"]
+        let myGame = EightBallModel(extraResponseArray: extraR);
+        
+        /* 1.6*/
+        print("Who gives you money?")
+        print(myGame.responseArray![0])
+        
+        print("Who feeds you?")
+        print(myGame.responseArray![1])
+        
+        print("Who protects you from the Mafia?")
+        print(myGame.responseArray![2])
+        
+        let questions = ["hey, Who gives you money?","hey, Who feeds you?","hey, Who protects you from the Mafia?"]
+        
+        //1.7
+        for (index,question) in questions.enumerate(){
+            print(question)
+            print(myGame.responseArray![index])
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
